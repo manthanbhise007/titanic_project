64665a7e2fc23f6ce8059877fffe8a2413f7bb66
Manthan Bhise <bhisemanthan985@gmail.com>
2026-04-25 18:57:26 +0530

    custom exception

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+from logger import logging
+
+def error_detail_message(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in [{0}] at line [{1}] and the error is [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_detail_message(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("divided by zero")
+        raise CustomException(e, sys)
